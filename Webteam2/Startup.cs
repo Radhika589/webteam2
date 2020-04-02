5a40a950683303d8e9d0c249ef3fdd55f598f4c3
John Andersson <john.andersson1995@gmail.com>
2020-04-02 11:08:00 +0200

    WEB2-51 add methods for depency injection

--- Webteam2/Startup.cs
+++ Webteam2/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Webteam2.Models;
 
 namespace Webteam2
 {
@@ -23,6 +24,9 @@ namespace Webteam2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMVC();
+
+            services.AddDbContext<Context>();
             services.AddControllersWithViews();
         }
 
