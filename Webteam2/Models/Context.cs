bcc4f32d659d47f75ce11f010abf111ecc578e5b
John Andersson <john.andersson1995@gmail.com>
2020-04-02 10:24:06 +0200

    WEB2-51 init EF
    add NuGets
    create Context.cs

---
+++ Webteam2/Models/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webteam2.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base (options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Warning!
+            //To protect potentially sensitive information in your connection string, you should move it out of source code.
+            //See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
