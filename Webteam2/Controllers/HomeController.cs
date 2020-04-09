b901bcd9ad403965673ba25257b325720c603082
Adde <de-dreas@hotmail.com>
2020-04-09 14:53:06 +0200

    Revert "WEB2-49 loginUI + navbar"
    This reverts commit a88be5831bb94c88633ab7711542be47b38690d8.

--- Webteam2/Controllers/HomeController.cs
+++ Webteam2/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace Webteam2.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
