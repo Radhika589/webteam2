496114ebec70f51adc2d12358f2214718a3e59d9
John Andersson <john.andersson1995@gmail.com>
2020-04-28 12:36:58 +0200

    add classes for profile feature, rename namespaces

---
+++ Webteam2/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webteam2
+{
+    public class Profile
+    {
+        public string Id { get; set; }
+        public string PictureURL { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public virtual User User { get; set; }
+    }
+}
