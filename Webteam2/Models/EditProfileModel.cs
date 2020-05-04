1dd112535506e43c7820dc4f62406037e1291348
John Andersson <john.andersson1995@gmail.com>
2020-05-04 15:08:13 +0200

    WEB-2-104 finish profile editing

--- Webteam2/Models/EditProfileModel.cs
+++ Webteam2/Models/EditProfileModel.cs
@@ -4,7 +4,7 @@ namespace Webteam2.Models
 {
     public class EditProfileModel
     {
-        [Required(ErrorMessage = "Write something about yourself!")]
+        [Required(ErrorMessage = "You cannot enter an empty description, write something about yourself!")]
         public string Description { get; set; }
         public Profile UserProfile { get; set; }
     }
