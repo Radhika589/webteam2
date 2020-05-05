feb9459021e7bb8f439f35dc841b9968e412d36b
Khosro <Jadidi>
2020-05-05 16:49:41 +0200

    WEB2-131 added correct row selection to jobs table

--- Webteam2/wwwroot/js/JobsList.js
+++ Webteam2/wwwroot/js/JobsList.js
@@ -1,20 +1,28 @@
 ﻿window.addEventListener("load",() => {
-    $('#jobs_table').DataTable({
-        "ajax": "/job/getall/",
-        dom: 'Bfrtip',
-        buttons: {
-            buttons: [
-                {
-                    //extend: "print",
-                    //autoPrint: false,
-                    text: 'Print Selected Row(chanage to "Accept Job" later)',
-                    action: function (e, dt, node, config) {
-                        //https://datatables.net/reference/option/buttons.buttons.action
-                        //https: //stackoverflow.com/questions/31309801/using-url-action-and-route-data-in-javascript
-                        var url = `@Url.Action("PreviewContract","Job",new{Area="",Id=${node}})`;
+        var table = $('#jobs_table').DataTable({
+            "ajax": "/job/getall/",
+            dom: 'Bfrtip',
+            buttons: {
+                buttons: [
+                    {
+                        //extend: "print",
+                        //autoPrint: false,
+                        text: 'Suggest A Price',
+                        action: function(e, dt, node, config) {
+                            //https://datatables.net/reference/option/buttons.buttons.action
+                            //https: //stackoverflow.com/questions/31309801/using-url-action-and-route-data-in-javascript
+                            //https://datatables.net/reference/type/selector-modifier
 
-                        //console.log(node);
-                    },
+                            let selectedRow = table.row({ selected: true }).data();
+                            //var url = Url.Action("PreviewContract","Job",new{id:selectedRow});
+                                
+                            var data = table.buttons.exportData({modifier:{selected:true}});
+
+                            console.log(data);
+                            
+
+
+                        },
                     exportOptions: {
                         modifier: {
                             selected: true
@@ -38,17 +46,3 @@
     });
 });
 
-//buttons: {
-//    buttons: [
-//        {
-//            extend: "print",
-//            autoPrint: false,
-//            text: 'Print Selected Row(chanage to "Accept Job" later)',
-//            exportOptions: {
-//                modifier: {
-//                    selected: true
-//                }
-//            }
-//        }
-//    ]
-//}
