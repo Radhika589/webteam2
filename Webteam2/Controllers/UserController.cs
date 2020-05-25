21d0f2494858ec11f9020a9cf174ed5f9c2bd9ae
Khosro <Jadidi>
2020-05-25 20:59:09 +0200

    WEB2-191 added serach functionality to the user list
    the index actionmethod in the userController has been updated. some html code has been added to the view file.

--- Webteam2/Controllers/UserController.cs
+++ Webteam2/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 using Webteam2.Models;
 
 namespace Webteam2.Controllers
@@ -13,36 +14,79 @@ namespace Webteam2.Controllers
     {
         private readonly Context _db;
         private readonly UserManager<User> _userManager;
+
         public UserController(Context db, UserManager<User> userManager)
         {
             _db = db;
             _userManager = userManager;
         }
-        
 
         #region API Calls
+
         [HttpGet]
         public async Task<IActionResult> Getall()
         {
             if (_db.Users.Any())
             {
-                return Json(new { data = await _db.Users.ToListAsync()});
+                return Json(new { data = await _db.Users.ToListAsync() });
             }
             else
             {
-                return Json(new { success=false,message="No Users Are Available."});
+                return Json(new { success = false, message = "No Users Are Available." });
             }
         }
+
+#nullable enable
         [HttpGet]
-        public IActionResult Index(UserLists userLists)
+        public IActionResult Index(string? searchString)
         {
-            userLists.Customers = _userManager.GetUsersInRoleAsync("Customer").Result;
-            userLists.NotValidatedContractors = _userManager.GetUsersInRoleAsync("NotValidatedContractor").Result;
-            userLists.ValidatedContractors = _userManager.GetUsersInRoleAsync("ValidatedContractor").Result;
-            userLists.Administrators = _userManager.GetUsersInRoleAsync("Administrator").Result;
+            
+            var userLists = new UserLists
+            {
+                Customers = _userManager.GetUsersInRoleAsync("Customer").Result,
+                NotValidatedContractors = _userManager.GetUsersInRoleAsync("NotValidatedContractor").Result,
+                ValidatedContractors = _userManager.GetUsersInRoleAsync("ValidatedContractor").Result,
+                Administrators = _userManager.GetUsersInRoleAsync("Administrator").Result
+            };
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.Substring(0, 1).ToUpper()
+                               + searchString.Substring(1, searchString.Length - 1).ToLower();
+
+                userLists.Customers = userLists.Customers
+                    .Where(
+                       c => c.FirstName.Contains(searchString)
+                       || c.LastName.Contains(searchString)
+                       || c.Location.Contains(searchString)
+                    ).ToList();
+
+                userLists.NotValidatedContractors = userLists.NotValidatedContractors
+                    .Where(
+                        c => c.FirstName.Contains(searchString)
+                             || c.LastName.Contains(searchString)
+                             || c.Location.Contains(searchString)
+                    ).ToList();
+
+                userLists.ValidatedContractors = userLists.ValidatedContractors
+                    .Where(
+                        c => c.FirstName.Contains(searchString)
+                             || c.LastName.Contains(searchString)
+                             || c.Location.Contains(searchString)
+                    ).ToList();
+
+                userLists.Administrators = userLists.Administrators
+                    .Where(
+                        c => c.FirstName.Contains(searchString)
+                             || c.LastName.Contains(searchString)
+                             || c.Location.Contains(searchString)
+                    ).ToList();
+            }
+
             return View(userLists);
             //return RedirectToPage(nameof(AccountController.ValidateContractors), contractorsToValidateModel);
         }
-        #endregion
+#nullable disable
+
+        #endregion API Calls
     }
 }
