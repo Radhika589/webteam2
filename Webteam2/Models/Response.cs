0874218ac499ad2effb9a46c3fe90812f59281d4
Khosro <Jadidi>
2020-05-27 15:13:20 +0200

    WEB-100 added a response class+added nuget packages

---
+++ Webteam2/Models/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Webteam2.Models
+{
+    public class Response
+    {
+        [JsonProperty("rates")]
+        public Dictionary<string, double> Rates { get; set; }
+
+        [JsonProperty("base")]
+        public string Base { get; set; }
+
+        [JsonProperty("date")]
+        public DateTimeOffset Date { get; set; }
+    }
+}
