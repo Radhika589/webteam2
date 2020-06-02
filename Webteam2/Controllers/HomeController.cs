f43b54ef22bbefd95fc7ce15446704369008ad66
Daniel <daniel.andersson@iths.se>
2020-06-02 09:05:29 +0200

    removed home from navbar and autoredirect to issues when loggedin

--- Webteam2/Controllers/HomeController.cs
+++ Webteam2/Controllers/HomeController.cs
@@ -9,15 +9,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Webteam2.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Webteam2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<User> _signInManager;
+        public HomeController(ILogger<HomeController> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
         public IActionResult Seed()
         {
@@ -25,6 +28,10 @@ namespace Webteam2.Controllers
         }
         public IActionResult Index()
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction(nameof(IssuesController.Index), "Issues");
+            }
             return View("Index");
         }
         public IActionResult Privacy()
